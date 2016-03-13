1e05e64b8cc4504b69c15fa81618384e74f9483a
jamesw <jamesw@deeson.co.uk>
2016-03-13 14:39:48 +0000

    tidy

---
+++ README.md
@@ -0,0 +1,11 @@
+dev server at localhost:8080 when you do :
+`npm start`
+
+build and deploy to surge :
+`npm run deploy`
+
+
+webpack + webpack-dev-server + bootstrap-loader + es6 modules
+
+based on the basic example from bootstrap-loader :
+https://github.com/shakacode/bootstrap-loader/tree/master/examples/basic
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+'use strict';
+/* eslint no-console: [2, { allow: ["log", "error"] }] */
+
 const webpack = require('webpack');
-const WebpackDevServer = require("webpack-dev-server");
-const config = require("./webpack.config.js");
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.config.js');
 
 const chokidar = require('chokidar');
 
-config.entry.app.unshift("webpack-dev-server/client?http://localhost:8080/");
+config.entry.app.unshift('webpack-dev-server/client?http://localhost:8080/');
 
 const compiler = webpack(config);
 const server = new WebpackDevServer(compiler, {
@@ -13,27 +16,27 @@ const server = new WebpackDevServer(compiler, {
   noInfo: false,
   watchOptions: {
     aggregateTimeout: 300,
-    poll: 1000
+    poll: 1000,
   },
-  publicPath: "/assets/",
+  publicPath: '/assets/',
   stats: { colors: true },
 });
 
-chokidar.watch('./src/markup/*').on('all', (event, path) => {
-  server.sockWrite(server.sockets, "invalid");
-  server.sockWrite(server.sockets, "hash", "imaginary");
-  server.sockWrite(server.sockets, "ok");
+chokidar.watch('./src/markup/*').on('all', () => {
+  server.sockWrite(server.sockets, 'invalid');
+  server.sockWrite(server.sockets, 'hash', 'imaginary');
+  server.sockWrite(server.sockets, 'ok');
 });
 
-server.listeningApp.on('listening', (e) => {
+server.listeningApp.on('listening', () => {
   console.log('listing on 8080');
 });
 
 server.listeningApp.on('error', (e) => {
   if (e.code === 'EADDRINUSE') {
-    console.log('You have another dev server running somewhere, shut it down first');
+    console.error('You have another dev server running somewhere, shut it down first');
   } else {
-    console.log('error', e);
+    console.error('error', e);
   }
   process.exit();
 });
